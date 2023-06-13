9a822f651963fcc36c5bdea000313bb1029fb2bc
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-06-13 21:02:30 +0100

    fibonachi

--- 11.3 TinDog Project/javaScript.js
+++ 11.3 TinDog Project/javaScript.js
@@ -7,7 +7,7 @@ console.log(arrayRandom1);*/
 
 
 
-/*second exercise*/
+/*second exercise
 
 var numberOfBottles = 99
 while (numberOfBottles >= 0) {
@@ -20,4 +20,25 @@ while (numberOfBottles >= 0) {
     console.log("Take one down, pass it around,");
 	numberOfBottles--;
     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
-}
+}*/
+
+
+function fibonachi(n){
+    var output=[];
+    if (n===1){
+        output=[0];
+    }else if (n=2){
+        output=[0,1];
+    }
+        else {
+            output=[0,1]
+            for (let i=2; i<=n; i++){
+                output.push(output[output.length-2]+output[output.length-1]);
+            }
+        }
+        return output
+    }
+    console.log(fibonachi(3));
+        
+    
+
