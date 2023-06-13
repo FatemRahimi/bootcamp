8885deca871dc5a6f92637abb184591daf087dc6
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-06-13 19:53:07 +0100

    bottle exercise

--- 11.3 TinDog Project/javaScript.js
+++ 11.3 TinDog Project/javaScript.js
@@ -1,6 +1,23 @@
-name =["apple", "orange", "cherry", "pear", "tengarin", "pulm" ];
+/*name =["apple", "orange", "cherry", "pear", "tengarin", "pulm" ];
 
 lengthArray = name.length;
 arrayRandom = Math.random()*lengthArray;
 arrayRandom1 = [arrayRandom];
-console.log(arrayRandom1);
+console.log(arrayRandom1);*/
+
+
+
+/*second exercise*/
+
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
