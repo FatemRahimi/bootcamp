7ed1347ce450f4b45d3ec575b24789bb6b26a4ea
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-06-15 16:14:01 +0100

    2 dices changed

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -8,3 +8,13 @@ var randomImageSource="images/" + randomDiceImage;
 image1= document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource)
 
+var randomNum1 = Math.floor(Math.random() * 6) + 1;
+
+// Construct the dice name based on the random number
+var randomDiceImage = "dice" + randomNum1 + ".png";
+//construct the full name base on the random number
+var randomImageSource2="images/" + randomDiceImage;
+// Assuming you have an <img> element with an id of 'diceImg'
+image1= document.querySelectorAll("img")[1];
+image1.setAttribute("src", randomImageSource2)
+
