801ff88d674e4c610f16e30fd5bea210fb98e4a4
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-07-14 15:09:07 +0000

    add promt and answer for inquirer moduale

--- node/QR/index.js
+++ node/QR/index.js
@@ -1,11 +1,32 @@
-CD /* 
+/* 
 1. Use the inquirer npm package to get user input.
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
-const fs = require("fs");
-fs.writeFile("new.txt", "say hello", (err)=>{
-    if(err) throw err;
-    console.log("saved");
-})
 
+import inquirer from "inquirer";
+
+inquirer
+  .prompt([
+    {
+      message: "Type in your URL: ",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
