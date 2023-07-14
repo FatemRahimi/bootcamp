e4c7eccc904ce8a9dd24b79cd171435d9a14135b
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-07-14 10:47:40 +0000

    fs.readFile practice

--- node/index.js
+++ node/index.js
@@ -1,6 +1,12 @@
 const fs= require("fs");
 
-fs.writeFile("Message.txt", "hello world", (err)=>{
+//fs.writeFile("Message.txt", "hello world", (err)=>{
+  //  if (err) throw err;
+  //  console.log("saved")
+//})
+
+
+fs.readFile("./Message.txt","utf-8",  (err,data)=>{
     if (err) throw err;
-    console.log("saved")
-})gir
+    console.log(data)
+})
