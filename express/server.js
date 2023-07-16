134ba2fa164ef3e9b974014828cbd8f9dbb24938
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-07-16 22:39:33 +0100

    add two number using body parser

--- express/server.js
+++ express/server.js
@@ -5,9 +5,15 @@ const app= express();
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html");
 })
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.post("/", function(req ,res){
-    res.send("thank you for sending");
+    var  n1=Number(req.body.num1);
+    var n2=Number(req.body.num2);
+
+     const number = n1 + n2;
+    res.send("the sum is" + number);
+
 })
 
 app.listen(3000, function(){
