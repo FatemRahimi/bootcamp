7d943f92e4679d57ad478d0d5b5af255a0d8385c
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-07-16 21:08:30 +0100

    install body-parser for post request

--- express/server.js
+++ express/server.js
@@ -1,5 +1,15 @@
-const express= require("express");
+const express = require("express");
+const bodyParser = require("body-parser");
 const app= express();
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req ,res){
+    res.send("thank you for sending");
+})
+
 app.listen(3000, function(){
     console.log("i can listen you");
 } )
